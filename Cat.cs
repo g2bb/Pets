cae542d09af1175dab014bc147ed0945cff731b5
Joe <g2bb@aol.com>
2019-03-07 20:56:23 -0600

    Add files via upload

---
+++ Cat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pets {
+    class Cat : Pet {
+        public Cat(string name, string owner, double weight) : base("Cat", name, owner, weight) { }
+
+        public string meow(int count) {
+            string number = "";
+            for (int i = 0; i < count; i++) {
+                number += "Meow! ";
+            }
+            return number;
+        }
+    }
+}
---
+++ Pets.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Pets {
+    class Pet {
+        public string type;
+        public string name;
+        public string owner;
+        public double weight;
+
+        public Pet (string type, string name, string owner, double weight) {
+            this.type = type;
+            this.name = name;
+            this.owner = owner;
+            this.weight = weight;
+        }
+
+        public string readTag() {
+            return String.Format("If lost, please call {0}", this.owner);
+        }
+    }
+}
